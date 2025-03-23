e7d2a77bb63699759b828aac025c1cd84020c51a
Harry23457 <southwelle@hotmail.com>
2025-03-23 08:14:36 +0000

    Add files via upload

--- birthday card.py
+++ birthday card.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 pygame.init()
 screen=pygame.display.set_mode((600,600))
@@ -15,4 +16,22 @@ while True:
     screen.blit (image1,(0,0))
     screen.blit(text,(150,200))
     pygame.display.update()
+    time.sleep(2)
+    
+    image2 =pygame.image.load("birthday present.jpg")
+    font=pygame.font.SysFont("comic sans",40)
+    text=font.render("Have a great day",True,"black")
+    screen.blit (image2,(0,0))
+    screen.blit(text,(100,100))
+    pygame.display.update()
+    time.sleep(2)
+
+    imag =pygame.image.load("birthday cake.jpg")
+    font=pygame.font.SysFont("comic sans",40)
+    text=font.render("May your wishes come true",True,"black")
+    screen.blit (imag,(0,0))
+    screen.blit(text,(50,50))
+    pygame.display.update()
+    time.sleep(2)
+
 
