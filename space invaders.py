e3103939b1e107ba874d71fdfd74328bf675da45
Harry23457 <southwelle@hotmail.com>
2025-06-15 08:57:18 +0100

    Add files via upload

--- space invaders.py
+++ space invaders.py
@@ -8,10 +8,12 @@ bg=pygame.transform.scale(pygame.image.load("space ship land.png"),(700,700))
 border=pygame.Rect(345,0,30,700)
 rhealth=10
 yhealth=10
+yellow=pygame.Rect(100,350,50,50)
+red=pygame.Rect(600,350,50,50)
 def draw():
     screen.blit(bg,(0,0))
-    screen.blit(image1,(100,350))
-    screen.blit(image2,(600,350))
+    screen.blit(image1,(yellow.x,yellow.y))
+    screen.blit(image2,(red.x,red.y))
     pygame.draw.rect(screen,"black",border)
     font=pygame.font.SysFont("times new roman",40)
     text1=font.render("Health:"+str(rhealth),True,"white")
@@ -19,13 +21,37 @@ def draw():
     text2=font.render("Health:"+str(yhealth),True,"white")
     screen.blit(text2,(50,50))
 
+def yellow_move(keys):
+    if keys[pygame.K_a] and yellow.x>0:
+        yellow.x-=5
+    if keys[pygame.K_d] and yellow.x<290:
+        yellow.x+=5
+    if keys[pygame.K_w] and yellow.y>0:
+        yellow.y-=5
+    if keys[pygame.K_s] and yellow.y<650:
+        yellow.y+=5
+
+def red_move(keys):
+    if keys[pygame.K_RIGHT] and red.x<650:
+        red.x+=5
+    if keys[pygame.K_LEFT] and red.x>380:
+        red.x-=5
+    if keys[pygame.K_UP] and red.y>0:
+        red.y-=5
+    if keys[pygame.K_DOWN] and red.y<650:
+        red.y+=5
     
+    
+          
 
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             exit()
     draw()
+    keys=pygame.key.get_pressed()
+    yellow_move(keys)
+    red_move(keys)
     pygame.display.update()
 
 
